b6f02cc908d113e6659f33d459b947986ad01e03
Johnny Jiang <Johnny.Jiang@suncorp.com.au>
2016-04-15 15:17:03 +1000

    [nj] added fetch().then().catch()

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,17 +1,50 @@
 'use stric'
 
+function startFormSubmissionResponse() {
+    console.log('startFormSubmissionResponse()');
+    return {
+        type: 'START_FORM_SUBMISSION_REQUEST'
+    }
+}
+
+function sendFormSubmissionRequest(formData) {
+    console.log('sendFormSubmissionRequest(formData)');
+    console.log(formData);
+    return {
+        type: 'SEND_FORM_SUBMISSION_REQUEST'
+    }
+}
+
+function getFormSubmissionResponse(response) {
+    console.log('getFormSubmissionResponse(response)');
+    console.log(response);
+    return {
+        type: 'GET_FORM_SUBMISSION_RESPONSE',
+        receivedAt: Date.now()
+    }
+}
+
+function getFormSubmissionError(error) {
+    console.log('getFormSubmissionError(error)');
+    console.log(error);
+    return {
+        type: 'GET_FORM_SUBMISSION_ERROR',
+        receivedAt: Date.now()
+    }
+}
+
 var formSubmitActionCreator = function (formData) {
     console.log('formSubmitActionCreator');
 
     return function(dispatch) {
-        setTimeout(function() {
-            console.log("async action dispatching, formData is:");
-            console.log(formData);
-            dispatch({
-                type: 'formSubmit',
-                formData
-            })
-        }, 5000)
+        dispatch(startFormSubmissionResponse());
+        return fetch('http://localhost:8000')
+        .then((response) => {
+            dispatch(getFormSubmissionResponse(response))
+        })
+        .catch((error) => {
+            dispatch(getFormSubmissionError(error))
+        })
     }
 }
 
